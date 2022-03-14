793386932cda8e389c2c4f8e2c231762969fd9f2
KKOLLANA <99629924+KKOLLANA@users.noreply.github.com>
2022-03-14 11:36:59 +0530

    Added spring core

---
+++ SpringCore/src/com/cg/Airtel.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+public class Airtel implements Sim {
+	@Override
+public void calling() {
+System.out.println("calling using airtel sim");
+}
+	@Override
+public void data() {
+System.out.println("browsing internet using airtel sim");
+
+	}
+
+}
